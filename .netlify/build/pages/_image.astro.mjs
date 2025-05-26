ea1c1822299c917f80f1d270a5b1e76f42fad047
Javier Rocamora <javier@loreapp.co>
2025-05-26 13:47:43 +0200

    Added puppeteer core to make it work with Netlify

--- .netlify/build/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_C2Q5_V55.mjs';
-export { renderers } from '../renderers.mjs';
--- src/pages/api/generate-pdf.ts
+++ src/pages/api/generate-pdf.ts
@@ -1,5 +1,6 @@
-import type { APIRoute } from 'astro';
-import puppeteer from 'puppeteer';
+import type { APIRoute } from "astro";
+import puppeteer from "puppeteer-core";
+import chromium from "chrome-aws-lambda";
 
 // Enable SSR endpoint
 export const prerender = false;
@@ -9,25 +10,32 @@ export const POST: APIRoute = async ({ request }) => {
     // Get the HTML content from the request
     const { html } = await request.json();
 
+    const chromeExe = import.meta.env.CHROME_EXECUTABLE_PATH;
     // Launch a headless browser
-    const browser = await puppeteer.launch();
+    // const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      executablePath: chromeExe || (await chromium.executablePath),
+      ...(chromeExe ? {} : {args: chromium.args}),
+      headless: true,
+    });
+
     const page = await browser.newPage();
 
     // Set the HTML content
     await page.setContent(html, {
-      waitUntil: 'networkidle0'
+      waitUntil: "networkidle0",
     });
 
     // Generate PDF
     const pdf = await page.pdf({
-      format: 'A4',
+      format: "A4",
       printBackground: true,
       margin: {
-        top: '1cm',
-        right: '1cm',
-        bottom: '1cm',
-        left: '1cm'
-      }
+        top: "1cm",
+        right: "1cm",
+        bottom: "0.1cm",
+        left: "1cm",
+      },
     });
 
     // Close the browser
@@ -37,17 +45,17 @@ export const POST: APIRoute = async ({ request }) => {
     return new Response(pdf, {
       status: 200,
       headers: {
-        'Content-Type': 'application/pdf',
-        'Content-Disposition': 'attachment; filename="factura.pdf"'
-      }
+        "Content-Type": "application/pdf",
+        "Content-Disposition": 'attachment; filename="factura.pdf"',
+      },
     });
   } catch (error) {
-    console.error('Error generating PDF:', error);
-    return new Response(JSON.stringify({ error: 'Failed to generate PDF' }), {
+    console.error("Error generating PDF:", error);
+    return new Response(JSON.stringify({ error: "Failed to generate PDF" }), {
       status: 500,
       headers: {
-        'Content-Type': 'application/json'
-      }
+        "Content-Type": "application/json",
+      },
     });
   }
 };
